d2182eb8dc5164b15677b5e8f472a80e2113c676
Victor Magana <vicmagana@hotmail.com>
2017-03-13 23:12:50 -0700

    adding Readme file
    adding Readme file for description of tests

--- Selenium_PageObject/pageObjects.py
+++ Selenium_PageObject/pageObjects.py
@@ -4,31 +4,30 @@ from selenium.webdriver.common.alert import Alert
 from selenium.webdriver.support.ui import WebDriverWait
 from basePage import BasePage
 import users, time, os
-from pageLocators import *
+from pageLocators import MainPageLocators
 from selenium.webdriver.support.select import Select
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
-import pywinauto
+
 
 class MainPage(BasePage):
-    def check_page_loaded(self):
-        return True if self.find_element_sub(*MainPageLocators.LOGO) else False
 
-    def search_item(self, item):
-        self.find_element_sub(*MainPageLocators.SEARCH).send_keys(item)
-        self.find_element_sub(*MainPageLocators.SEARCH).send_keys(Keys.ENTER)
-        return self.find_element_sub(*MainPageLocators.SEARCH_LIST).text
+    def __init__(self):
+        self.MainPage_Locators = MainPageLocators()
+
+    def check_page_loaded(self):
+        return True if self.find_element_sub(*self.MainPage_Locators.LOGO) else False
 
     def set_firstname(self,firstname):
-        self.find_element_sub(*MainPageLocators.FIRSTNAME).send_keys(firstname)
+        self.find_element_sub(*self.MainPage_Locators.FIRSTNAME).send_keys(firstname)
         return True
 
     def set_lastname(self,lastname):
-        self.find_element_sub(*MainPageLocators.LASTTNAME).send_keys(lastname)
+        self.find_element_sub(*self.MainPage_Locators.LASTTNAME).send_keys(lastname)
         return True
 
     def select_male_female(self,gender):
-        elem_list_results = self.find_elements_sub(*MainPageLocators.MALE_FEMALE)
+        elem_list_results = self.find_elements_sub(*self.MainPage_Locators.MALE_FEMALE)
         for item in elem_list_results:
             if item.get_attribute('value') == gender:
                 item.click()
@@ -36,7 +35,7 @@ class MainPage(BasePage):
         return False
 
     def select_years_experience(self,years):
-        elem_list_years = self.find_elements_sub(*MainPageLocators.YEARS_EXPERIENCE)
+        elem_list_years = self.find_elements_sub(*self.MainPage_Locators.YEARS_EXPERIENCE)
         for year in elem_list_years:
             if year.get_attribute('value') == years:
                 year.click()
@@ -44,7 +43,7 @@ class MainPage(BasePage):
         return False
 
     def select_date(self,date_data):
-        elem_date = self.find_element_sub(*MainPageLocators.DATE)
+        elem_date = self.find_element_sub(*self.MainPage_Locators.DATE)
         elem_date.clear()
         elem_date.send_keys(date_data)
         get_date = elem_date.get_attribute('value')
@@ -53,7 +52,7 @@ class MainPage(BasePage):
         return False
 
     def select_profession(self,profession):
-        elem_professions = self.find_elements_sub(*MainPageLocators.PROFESSION)
+        elem_professions = self.find_elements_sub(*self.MainPage_Locators.PROFESSION)
         for job in elem_professions:
             if job.get_attribute('value') == profession:
                 job.click()
@@ -61,7 +60,7 @@ class MainPage(BasePage):
         return False
 
     def select_automation_tools(self,tool):
-        elem_tools = self.find_elements_sub(*MainPageLocators.TOOL)
+        elem_tools = self.find_elements_sub(*self.MainPage_Locators.TOOL)
         for entry in elem_tools:
             if entry.get_attribute('value') == tool:
                 entry.click()
@@ -69,14 +68,14 @@ class MainPage(BasePage):
         return False
 
     def select_continents(self,continent):
-        Select(self.find_element_sub(*MainPageLocators.CONTINENTS)).select_by_visible_text(continent)
-        elem_option = Select(self.find_element_sub(*MainPageLocators.CONTINENTS)).first_selected_option
+        Select(self.find_element_sub(*self.MainPage_Locators.CONTINENTS)).select_by_visible_text(continent)
+        elem_option = Select(self.find_element_sub(*self.MainPage_Locators.CONTINENTS)).first_selected_option
         if elem_option.text == continent:
             return True
         return False
 
     def set_selenium_cmds(self,commands):
-        elem_options = Select(self.find_element_sub(*MainPageLocators.SELENIUM_CMDS)).options
+        elem_options = Select(self.find_element_sub(*self.MainPage_Locators.SELENIUM_CMDS)).options
         for item in elem_options:
             if item.get_attribute('value') == commands:
                 item.click()
@@ -84,7 +83,7 @@ class MainPage(BasePage):
         return False
 
     def download_file(self,filename):
-        self.find_element_sub(*MainPageLocators.DOWNLOAD_FILE).click()
+        self.find_element_sub(*self.MainPage_Locators.DOWNLOAD_FILE).click()
         time.sleep(2)
         fpath = os.getcwd() + '\\' + filename
         if os.path.isfile(fpath):
@@ -94,7 +93,7 @@ class MainPage(BasePage):
 
     def upload_file(self,filename):
         fpath = os.getcwd() + '\\' + filename
-        elem_upload_file = self.find_element_sub(*MainPageLocators.UPLOAD_FILE)
+        elem_upload_file = self.find_element_sub(*self.MainPage_Locators.UPLOAD_FILE)
         elem_upload_file.clear()
         elem_upload_file.send_keys(fpath)
         elem_name = elem_upload_file.get_attribute('value')
@@ -102,15 +101,6 @@ class MainPage(BasePage):
             return True
         else:
             return False
-
-    def uploadTest(self,Name_of_File):
-        app = pywinauto.application.Application().start()
-        mainWindow = app['Upload']  # main windows' title
-        #ctrl = mainWindow['Edit']
-        mainWindow.SetFocus()
-        mainWindow.TypeKeys('TestFile')
-        ctrlBis = mainWindow['TestFileSection']  # open file button
-        ctrlBis.ClickInput()
 
 
     def is_element_present(self, how, what):
@@ -138,30 +128,6 @@ class MainPage(BasePage):
             return alert_text
         finally:
             self.accept_next_alert = True
-
-
-class LoginPage(BasePage):
-    def enter_email(self,user):
-        self.find_element_sub(*LoginPageLocators.EMAIL).send_keys(users.get_user(user)['email'])
-
-    def enter_password(self,user):
-        self.find_element_sub(*LoginPageLocators.PASSWORD).send_keys(users.get_user(user)['password'])
-
-    def click_login_button(self):
-        self.find_element_sub(*LoginPageLocators.SUBMIT).click()
-
-    def login(self,user):
-        self.enter_email(user)
-        self.enter_password(user)
-        self.click_login_button()
-
-    def login_with_valid_user(self,user):
-        self.login(user)
-        return HomePage(self.driver)
-
-    def login_with_invalid_user(self,user):
-        self.login(user)
-        return self.find_element_sub(*LoginPageLocators.ERROR_MESSAGE).text
 
 
 class HomePage(BasePage):
